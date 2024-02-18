578a06305d4676d28eb8dc6f81f726dc4c579b67
João <jvlprodocimo@gmail.com>
2024-02-18 16:50:01 -0300

    js

---
+++ assets/js/sandwich.js
@@ -0,0 +1,10 @@
+ const menu = document.getElementById('menu') 
+ const xMenu = document.getElementById('xMenu')
+ 
+ function clickMenu() { 
+        menu.style.display = 'block'
+    }
+
+ function clickX() {
+        menu.style.display = 'none'
+    }
